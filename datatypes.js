34f4c35428cb542d3060510b12ca474239aedcde
js-arsalan <oarsalan17@gmail.com>
2025-02-11 20:10:15 +0500

    ok

---
+++ datatypes.js
@@ -0,0 +1,27 @@
+"use strict"; // treat all js code as newer version
+
+//alert (3 + 3) we are using nod js, not browser
+
+console.log (3
+    +3)// code redabillity should be high
+
+console.log("arsalan")
+
+let name = "arsalan"
+let age = 18
+let isloggedin = false
+let state;
+
+// number => 2 to power of 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => stand alone value
+// undefined =>
+// symbol => unique
+
+// object
+
+
+console.log(typeof undefined); //undefined
+console.log(typeof null); // object
---
+++ variables.js
@@ -0,0 +1,20 @@
+const accountId = 144553
+ let accountemail = "oarsalan17@gmail.com"
+ var accountpassword = "12345"
+ accountcity ="karachi"
+ let accountstate;
+
+//  accountid = 2// not allowed
+accountemail ="hc@hc.com"
+accountpassword = "2121211"
+accountcity = "islamabad"
+
+ console.log (accountId);
+ console.table ([accountId,accountemail, accountpassword,accountcity,accountstate])
+
+ /*
+ prefer not use var 
+ because of issue in block scope and functional scope
+
+ */
+
