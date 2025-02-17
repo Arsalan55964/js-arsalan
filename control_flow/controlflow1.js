fd26e1a04a187534651b5a846f8df27f39e0aebb
js-arsalan <oarsalan17@gmail.com>
2025-02-18 00:03:55 +0500

    hogya

---
+++ control_flow/controlflow1.js
@@ -0,0 +1,51 @@
+// if
+
+
+const isUserloggedIn = true
+const temprature = 41
+
+
+// if ( temprature === 40){
+//     console.log("less than 50");
+    
+// } else{
+//     console.log(" temprature greator than 50");
+// }
+
+// console.log("executed");
+
+
+//  <,  >, <=, =>, ==, ===, !=, !==
+
+const score = 200
+
+// if (score > 100) {
+//     let  power = "fly"
+//     console.log(`arsalan power: ${power}`); is conditions ky andr kabhi var use nhi krna 
+    
+// }
+//  console.log(`arsalan power: ${power}`); var use krny ye console bhi run hota hy
+
+
+
+
+
+const balance = 1000
+
+// if (balance > 500) console.log("test"),console.log("test2"); is tarkky sy code run nhi krna ye outdated hy
+
+// 
+
+const UserloggedIn = true
+const debitCard = true
+const loggedInFromGoogle = false
+const loggedInFromEmail  = true
+
+if (UserloggedIn && debitCard ) {
+    console.log("Allow to buy course");
+    
+}
+if (loggedInFromGoogle ||  loggedInFromEmail) {
+    console.log("user logged in");
+    
+}
---
+++ control_flow/switch.js
@@ -0,0 +1,28 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = "march"
+
+switch (month) {
+    case "jan":
+        console.log("january");
+        break;
+    case "feb":
+        console.log("feb");
+        break;
+    case "march":
+        console.log("march");
+        break;
+    case "aprail":
+        console.log("aprail");
+        break;
+
+    default:
+        break;
+}
